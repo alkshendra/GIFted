3c99c61cbac5ae6236f824805c461370065e9b10
Alkshendra Maurya <alkshendra@mgrm.tech>
2019-06-01 10:19:34 +0530

    Make search form functional

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
 
 // Import styles
 import styles from './style.module.scss';
 
 class SearchInput extends Component {
+	state = {
+		searchPhrase: ''
+	};
+
+	handleInputChange = e => {
+		this.setState({ searchPhrase: e.currentTarget.value });
+	}
+
+	handleSearch = () => {
+		const { searchPhrase } = this.state;
+		if (searchPhrase !== '') {
+			this.props.history.push(`/search/${searchPhrase}`);
+		}
+	}
+
 	render() {
 		return (
-			<div className={styles.searchInputContainer}>
+			<div className={`${styles.searchInputContainer} ${
+				!!this.props.currentSearchTerm ? styles.minifiedView : ''
+			}`}>
 				<p className={styles.title}>GIFted</p>
 				<div className={styles.searchInputWrap}>
 					<input
+						onChange={this.handleInputChange}
 						className={styles.searchInput}
 						type="text"
 					/>
 					<button
+						onClick={this.handleSearch}
 						className={styles.searchAct}
 						type="button"
 					>
@@ -25,4 +45,4 @@ class SearchInput extends Component {
 	}
 }
 
-export default SearchInput;
+export default withRouter(SearchInput);
