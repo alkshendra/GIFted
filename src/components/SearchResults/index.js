761b146d320d08acdc2508de65c442bb46afc7a4
Alkshendra Maurya <alkshendra@mgrm.tech>
2019-06-05 08:00:49 +0530

    Add results loader + UI improvements to SearchResults

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -9,42 +9,61 @@ import styles from './style.module.scss';
 import Post from './Post.js';
 
 function SearchResults(props) {
-	const { currentSearchTerm, posts, clearSearch } = props;
+	const { searchTerm, posts, resultsLoading, clearSearch } = props;
+
+	// Reduce op. to flatten arrays of postsInAPage into a single array...
+	const postsArr = Object.values(posts).reduce(
+		(arr, postsInAPage) => postsInAPage.length > 0 ? arr.concat(postsInAPage) : arr,
+		[],
+	);
 	return (
 		<div className={styles.searchResults}>
-			<p className={styles.resultsMessage}>
-				<span>Showing results for</span>
-				<button className={styles.clearSearch} onClick={clearSearch}>
-					{currentSearchTerm}
-					<svg viewBox="0 0 30 30">
-						<path d="M 7.9785156 5.9804688 A 2.0002 2.0002 0 0 0 6.5859375 9.4140625 L 12.171875 15 L 6.5859375 20.585938 A 2.0002 2.0002 0 1 0 9.4140625 23.414062 L 15 17.828125 L 20.585938 23.414062 A 2.0002 2.0002 0 1 0 23.414062 20.585938 L 17.828125 15 L 23.414062 9.4140625 A 2.0002 2.0002 0 0 0 21.960938 5.9804688 A 2.0002 2.0002 0 0 0 20.585938 6.5859375 L 15 12.171875 L 9.4140625 6.5859375 A 2.0002 2.0002 0 0 0 7.9785156 5.9804688 z" />
-					</svg>
-				</button>
-			</p>
-			{posts && posts.length > 0
+			{postsArr.length > 0
 				? (
-					<Masonry
-						breakpointCols={{
-							default: 4,
-							1100: 3,
-							700: 2,
-							500: 1
-						}}
-						className={styles.postsWrap}
-						columnClassName="my-masonry-grid_column"
-					>
-						{posts.map(post => (
-							<Post
-								key={post.id}
-								title={post.title}
-								stillImgUrl={post.images['480w_still'].url}
-								stillImgLoadingUrl={post.images.fixed_height_small_still.url}
-								gifUrl={post.images.original.url}
-								gifLoadingUrl={post.images.fixed_height_small.url}
-							/>
-						))}
-					</Masonry>
-				) : ('loading')
+					<React.Fragment>
+						<p className={styles.resultsMessage}>
+							<span>Serving fresh brewed results for</span>
+							<button className={styles.clearSearch} onClick={clearSearch}>
+								{searchTerm}
+								<svg viewBox="0 0 30 30">
+									<use xlinkHref="/icons.svg#close" />
+								</svg>
+							</button>
+						</p>
+						<Masonry
+							breakpointCols={{
+								default: 4,
+								1100: 3,
+								700: 2,
+								500: 1
+							}}
+							className={styles.postsWrap}
+							columnClassName="my-masonry-grid_column"
+						>
+							{postsArr.map((post, i) => (
+								<Post
+									key={post.id}
+									title={post.title}
+									stillImgUrl={post.images['480w_still'].url}
+									stillImgLoadingUrl={post.images.fixed_height_small_still.url}
+									gifUrl={post.images.original.url}
+									gifLoadingUrl={post.images.fixed_height_small.url}
+								/>
+							))}
+						</Masonry>
+					</React.Fragment>
+				) : (
+					resultsLoading
+					? <div className={styles.resultsLoading} />
+					: (
+						<div className={styles.noResultsFound}>
+							<svg viewBox="0 0 30 30">
+								<use xlinkHref="/icons.svg#ghost" />
+							</svg>
+							<p>No results found</p>
+						</div>
+					)
+				)
 			}
 		</div>
 	)
